cd42922642d02e0be935e2b74fea3b487d9131c2
Denis.Kitrish <Denis.Kitrish@Yandex.ua>
2017-09-21 21:46:05 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ *	Класс для вычислений.
+ *	@author Denis.Kitrish
+ *	@since 20.09.2017
+ *	@version 1.0
+ */
 public class Calculate {
+	
+	/**
+	 *	Вывод строки в консоль.
+	 *	@params args Параметры из командной строки.
+	 */
 	public static void main(String[] args) {
 		System.out.println("I`m vary happy to live my life :)");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *	Package for calculate task/
+ *
+ *	@author Denis.Kitrish (Denis.Kitrish@Yandex.ua)
+ *	@since 21.09.2017
+ *	@version 1.0
+ */
+package ru.job4j;
