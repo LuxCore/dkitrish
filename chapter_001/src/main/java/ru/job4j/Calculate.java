c2d3bb407651ff38d206db5bbac38bec5163f8c5
Denis.Kitrish <Denis.Kitrish@Yandex.ua>
2017-09-21 22:28:09 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -10,9 +10,19 @@ public class Calculate {
 	
 	/**
 	 *	Вывод строки в консоль.
-	 *	@params args Параметры из командной строки.
+	 *	@param args Параметры из командной строки.
 	 */
 	public static void main(String[] args) {
 		System.out.println("I`m vary happy to live my life :)");
+	}
+	
+	/**
+	 *	Method echo.
+	 *
+	 *	@param name Sombody's name.
+	 *	@return Three times "echo" plus sombody's name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo: " + name;
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *	Test class.
+ *
+ *	@author Denis.Kitrish (Denis.Kitrish@Yandex.ua)
+ *	@since 21.09.2017
+ *	@version 1.0
+ */
+public class CalculateTest {
+	/**
+	 *	Test method.
+	 */
+	@Test
+	public void whenTakeNameThenThreeEchoPlusName() {
+		String input = "Denis Kitrish";
+		String expect = "Echo, echo, echo: Denis Kitrish";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+}
