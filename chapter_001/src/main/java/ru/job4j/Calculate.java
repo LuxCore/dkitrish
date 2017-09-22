6934a1c81535941dd36a70bff58376103c424752
Denis.Kitrish <Denis.Kitrish@Yandex.ua>
2017-09-22 21:58:47 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -7,15 +7,16 @@ package ru.job4j;
  *	@version 1.0
  */
 public class Calculate {
-	
+
 	/**
 	 *	Вывод строки в консоль.
+	 *
 	 *	@param args Параметры из командной строки.
 	 */
 	public static void main(String[] args) {
 		System.out.println("I`m vary happy to live my life :)");
 	}
-	
+
 	/**
 	 *	Method echo.
 	 *
