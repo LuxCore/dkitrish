ee4f83260635805d7b5fe60b2e0c4b38f4b31335
Denis.Kitrish <Denis.Kitrish@Yandex.ua>
2018-04-15 14:07:34 +0300

    5.3.1. Создать динамический список на базе массива. [#158]

---
+++ 2.Junior/001.CollectionsPRO/src/main/java/ru/job4j/list/DynamicArrayList.java
@@ -0,0 +1,146 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Динамический список на базе массива.
+ *
+ * @param <E> Тип элементов динамического массива.
+ */
+public class DynamicArrayList<E> implements Iterable<E> {
+	/**
+	 * Начальное значение размера массива container.
+	 */
+	private static final int INITIAL_CAPACITY = 16;
+
+	/**
+	 * Контейнер объектов.
+	 */
+	private Object[] container;
+
+	/**
+	 * Указатель на позицию в контейнере, куда нужно будет добавить объект.
+	 */
+	private int cursor;
+
+	/**
+	 * Количество изменений в контейнере объектов.
+	 */
+	private int modificationCount;
+
+	/**
+	 * Конструктор по умолчанию.
+	 * Задаёт начальный размер контейнера объектов равный 16.
+	 */
+	public DynamicArrayList() {
+		this.container = new Object[INITIAL_CAPACITY];
+	}
+
+	/**
+	 * Конструктор, задающий начальную длину контейнера.
+	 *
+	 * @param length Длина контейнера.
+	 */
+	public DynamicArrayList(int length) {
+		this.container = new Object[length];
+	}
+
+	/**
+	 * Добавление элемента в список.
+	 *
+	 * @param value Добавляемый объект.
+	 */
+	public void add(E value) {
+		ensureCapacity();
+		this.container[this.cursor++] = value;
+		this.modificationCount++;
+	}
+
+	/**
+	 * Получение объекта из списка.
+	 *
+	 * @param index Индекс массива, по которому можно найти искомый объект.
+	 * @return Искомый по индексу объект.
+	 */
+	@SuppressWarnings("unchecked")
+	public E get(int index) {
+		return (E) this.container[index];
+	}
+
+	/**
+	 * Получение количества объектов в контейнере.
+	 *
+	 * @return Количество элементов в контейнере.
+	 */
+	public int elementsCount() {
+		return this.cursor;
+	}
+
+	/**
+	 * Получение размера контейнера.
+	 *
+	 * @return Размер контейнера.
+	 */
+	public int size() {
+		return this.container.length;
+	}
+
+	/**
+	 * Убеждаемся в том, что в массив можно добавлять элементы.
+	 */
+	private void ensureCapacity() {
+		if (this.cursor == this.container.length) {
+			int newLength = this.container.length * 3 / 2 + 1;
+			this.container = Arrays.copyOf(this.container, newLength);
+		}
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new Itr();
+	}
+
+	/**
+	 * Итератор по контейнеру.
+	 */
+	private class Itr implements Iterator<E> {
+		/**
+		 * Индекс итератора.
+		 */
+		private int index;
+
+		/**
+		 * Количество изменений на момент создания итератора.
+		 * <p>Неизменяемая переменная.</p>
+		 */
+		private int modificationCount;
+
+		/**
+		 * Конструктор копирует значение количества изменений контейнера.
+		 */
+		Itr() {
+			this.modificationCount = DynamicArrayList.this.modificationCount;
+		}
+
+		@Override
+		public boolean hasNext() {
+			if (this.modificationCount != DynamicArrayList.this.modificationCount) {
+				throw new ConcurrentModificationException();
+			}
+			return this.index < DynamicArrayList.this.container.length
+					&& DynamicArrayList.this.container[this.index] != null;
+		}
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public E next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			return (E) DynamicArrayList.this.container[this.index++];
+		}
+	}
+}
---
+++ 2.Junior/001.CollectionsPRO/src/test/java/ru/job4j/list/DynamicArrayListTest.java
@@ -0,0 +1,166 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тестирование динамического списка на базе массива.
+ */
+public class DynamicArrayListTest {
+	/**
+	 * Тест добавления элементов с автоматическим расширением по достижении
+	 * предела размера контейнера объектов.
+	 */
+	@Test
+	public void testAdditionWithAutogrowth() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>();
+		for (int i = 0; i < 30;) {
+			dal.add(++i);
+		}
+		assertThat(dal.elementsCount(), is(30));
+		assertThat(dal.size(), is(38));
+	}
+
+	/**
+	 * Тест нормального итерирования по коллекции.
+	 */
+	@Test
+	public void testIterator() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>(5);
+		for (int i = 0; i < 6;) {
+			dal.add(++i);
+		}
+		Iterator<Integer> it = dal.iterator();
+		assertThat(it.next(), is(1));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.next(), is(2));
+		assertThat(it.next(), is(3));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.next(), is(4));
+		assertThat(it.next(), is(5));
+		assertThat(it.next(), is(6));
+	}
+
+	/**
+	 * Тест исключения во время работы итератора, когда в контейнере
+	 * происходят изменения и вызывается метод hasNext().
+	 */
+	@Test(expected = ConcurrentModificationException.class)
+	public void testConcurrentModificationExceptionWhenHasNextInvoked() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>(5);
+		for (int i = 0; i < 5;) {
+			dal.add(++i);
+		}
+		Iterator<Integer> it = dal.iterator();
+		assertThat(it.next(), is(1));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.next(), is(2));
+		dal.add(69);
+		it.hasNext();
+	}
+
+	/**
+	 * Тест исключения во время работы итератора, когда в контейнере
+	 * происходят изменения и вызывается метод next().
+	 */
+	@Test(expected = ConcurrentModificationException.class)
+	public void testConcurrentModificationExceptionWhenNextInvoked() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>(5);
+		for (int i = 0; i < 6;) {
+			dal.add(++i);
+		}
+		Iterator<Integer> it = dal.iterator();
+		assertThat(it.next(), is(1));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.hasNext(), is(true));
+		assertThat(it.next(), is(2));
+		dal.add(69);
+		it.next();
+	}
+
+	/**
+	 * Тест исключения во время работы итератора.
+	 */
+	@Test(expected = NoSuchElementException.class)
+	public void testIteratorNoSuchElementException() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>(5);
+		for (int i = 0; i < 6;) {
+			dal.add(++i);
+		}
+		Iterator<Integer> it = dal.iterator();
+		assertThat(it.next(), is(1));
+		assertThat(it.next(), is(2));
+		assertThat(it.next(), is(3));
+		assertThat(it.next(), is(4));
+		assertThat(it.next(), is(5));
+		assertThat(it.next(), is(6));
+		it.next();
+	}
+
+	/**
+	 * Тест исключения во время работы итератора.
+	 */
+	@Test
+	public void testIteratorHasNextFalse() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>(5);
+		for (int i = 0; i < 5;) {
+			dal.add(++i);
+		}
+		Iterator<Integer> it = dal.iterator();
+		assertThat(it.next(), is(1));
+		assertThat(it.next(), is(2));
+		assertThat(it.next(), is(3));
+		assertThat(it.next(), is(4));
+		assertThat(it.next(), is(5));
+		assertThat(it.hasNext(), is(false));
+	}
+
+	/**
+	 * Проверка получения объектов из контейнера.
+	 */
+	@Test
+	public void testGettingElement() {
+		DynamicArrayList<Character> dal = new DynamicArrayList<>(10);
+		dal.add('A');
+		dal.add('B');
+		dal.add('c');
+		dal.add('d');
+		dal.add('E');
+		assertThat(dal.get(0), is('A'));
+		assertThat(dal.get(1), is('B'));
+		assertThat(dal.get(2), is('c'));
+		assertThat(dal.get(3), is('d'));
+		assertThat(dal.get(4), is('E'));
+	}
+
+	/**
+	 * Проверка исключения, когда вызывается метод get(int) с отрицательным
+	 * значением индекса.
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGettingElementWithNegativeIndex() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>();
+		dal.add(69);
+		dal.get(-69);
+	}
+
+	/**
+	 * Проверка исключения, когда вызывается метод get(int) с превышающим
+	 * максимальное значение индекса.
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGettingElementWithIndexOverThenMaximum() {
+		DynamicArrayList<Integer> dal = new DynamicArrayList<>();
+		dal.add(69);
+		dal.get(69);
+	}
+}
