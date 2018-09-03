b15ee0452a752d7c95395e3ed5583c5abdaa4c90
Denis.Kitrish <Denis.Kitrish@Yandex.ua>
2018-09-03 23:53:39 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- 2.Junior/002.MultiThreading/src/test/java/ru/job4j/waitnotify/SimpleBlockingQueueTest.java
+++ 2.Junior/002.MultiThreading/src/test/java/ru/job4j/waitnotify/SimpleBlockingQueueTest.java
@@ -3,10 +3,46 @@ package ru.job4j.waitnotify;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Тестирование простой блокирующей очереди.
  */
 public class SimpleBlockingQueueTest {
+	/**
+	 * Тест на ожидание главным потоком окончания работы производителя
+	 * и потребителя.
+	 *
+	 * @throws InterruptedException Исключение прерывания.
+	 */
+	@Test
+	public void whenFetchAllThenGetIt() throws InterruptedException {
+		final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+		final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+		Thread producer = new Thread(() -> IntStream.range(0, 10).forEach(queue::offer));
+		producer.start();
+		Thread consumer = new Thread(() -> {
+			while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+				try {
+					buffer.add(queue.poll());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					Thread.currentThread().interrupt();
+				}
+			}
+		});
+		consumer.start();
+		producer.join();
+		consumer.interrupt();
+		consumer.join();
+		assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+	}
+
 	/**
 	 * Тест блокирующей очереди с помощью производтеля и потребителей.
 	 */
@@ -20,6 +56,7 @@ public class SimpleBlockingQueueTest {
 				System.out.println("Отправил: " + i);
 			}
 		}, "Producer");
+		producer.start();
 		Thread consumer = new Thread(() -> {
 			for (int i = 0; i < 15; i++) {
 				try {
@@ -29,7 +66,6 @@ public class SimpleBlockingQueueTest {
 				}
 			}
 		}, "Consumer");
-		producer.start();
 		consumer.start();
 		try {
 			producer.join();
