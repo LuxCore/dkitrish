4a19436478f29b7901c0f951fceaac4c5fe0553d
Denis.Kitrish <Denis.Kitrish@Yandex.ua>
2018-12-12 21:29:27 +0200

    4.1. Log4j 2. Логирование системы. [#95335]

---
+++ 2.Junior/003.SQL_JDBC/src/main/java/ru/job4j/logging/LogbackDemo.java
@@ -0,0 +1,28 @@
+package ru.job4j.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Logging demo.
+ */
+public class LogbackDemo {
+	/**
+	 * Logger.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(LogbackDemo.class);
+
+	/**
+	 * Main.
+	 *
+	 * @param args Console arguments.
+	 */
+	public static void main(String[] args) {
+		int version = 1;
+		LOG.trace("trace message {}", version);
+		LOG.debug("debug message {}", version);
+		LOG.info("info message {}", version);
+		LOG.warn("warn message {}", version);
+		LOG.error("error message {}", version);
+	}
+}
