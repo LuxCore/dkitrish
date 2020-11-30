8885b7e6df092f94ea816751361be6b65c5a50c6
LuxCore <Denis.Kitrish@Yandex.ua>
2020-12-01 01:09:53 +0200

    3. Уникальность элементов и сортировка. [#110225]

--- 1.Trainee/004.Lambda/src/main/java/ru/job4j/lambda/Profiles.java
+++ 1.Trainee/004.Lambda/src/main/java/ru/job4j/lambda/Profiles.java
@@ -8,11 +8,18 @@ import java.util.stream.Collectors;
  */
 public class Profiles {
 	/**
-	 * Извлечение адресов из всех анкет клиентов.
+	 * Извлечение адресов из всех анкет клиентов с подавлением
+	 * дублирующихся адресов.
 	 * @param profiles Анкета клиента турфирмы.
 	 * @return список адресов клиентов.
 	 */
 	public List<Address> collect(List<Profile> profiles) {
-		return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+		return profiles.stream()
+				.sorted((o1, o2) -> o1.getAddress().getCity().compareTo(
+						o2.getAddress().getCity()
+				))
+				.map(Profile::getAddress)
+				.distinct()
+				.collect(Collectors.toList());
 	}
 }
--- 1.Trainee/004.Lambda/src/test/java/ru/job4j/lambda/ProfilesTest.java
+++ 1.Trainee/004.Lambda/src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -14,11 +14,34 @@ public class ProfilesTest {
 	 * Проверка работы извлечения адресов клиентов.
 	 */
 	@Test
-	public void collect() {
+	public void testCollect() {
 		List<Profile> profiles = List.of(
 				new Profile(new Address("City 1", "Street 1", 1, 1)),
 				new Profile(new Address("City 2", "Street 2", 2, 2)),
 				new Profile(new Address("City 3", "Street 3", 3, 3)),
+				new Profile(new Address("City 4", "Street 4", 4, 4))
+		);
+		List<Address> expected = List.of(
+				new Address("City 1", "Street 1", 1, 1),
+				new Address("City 2", "Street 2", 2, 2),
+				new Address("City 3", "Street 3", 3, 3),
+				new Address("City 4", "Street 4", 4, 4)
+		);
+		assertEquals(expected, new Profiles().collect(profiles));
+	}
+
+	@Test
+	public void testCollectWhenSuppressDuplicates() {
+		List<Profile> profiles = List.of(
+				new Profile(new Address("City 3", "Street 3", 3, 3)),
+				new Profile(new Address("City 4", "Street 4", 4, 4)),
+				new Profile(new Address("City 2", "Street 2", 2, 2)),
+				new Profile(new Address("City 3", "Street 3", 3, 3)),
+				new Profile(new Address("City 4", "Street 4", 4, 4)),
+				new Profile(new Address("City 1", "Street 1", 1, 1)),
+				new Profile(new Address("City 4", "Street 4", 4, 4)),
+				new Profile(new Address("City 3", "Street 3", 3, 3)),
+				new Profile(new Address("City 2", "Street 2", 2, 2)),
 				new Profile(new Address("City 4", "Street 4", 4, 4))
 		);
 		List<Address> expected = List.of(
