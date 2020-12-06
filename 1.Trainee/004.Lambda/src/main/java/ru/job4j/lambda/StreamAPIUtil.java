5d99fa84b40bd43e2bee2ba20a189865cc8901b3
Denis.Kitrish <denis.kitrish@yandex.ua>
2020-12-07 00:17:49 +0200

    Оптимизация кода: перенос метода в другой класс и удаление ненужных классов.

--- 1.Trainee/004.Lambda/src/main/java/ru/job4j/lambda/StreamAPIUtil.java
+++ 1.Trainee/004.Lambda/src/main/java/ru/job4j/lambda/StreamAPIUtil.java
@@ -2,6 +2,7 @@ package ru.job4j.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -9,6 +10,15 @@ import java.util.stream.Collectors;
  * В данном классе собраны несколько задач из курсы.
  */
 public class StreamAPIUtil {
+	/**
+	 * Преобразование списка студентов в карту.
+	 * @param students Список студентов.
+	 * @return карта студентов.
+	 */
+	public Map<String, List<Student>> listToMap(List<Student> students) {
+		return students.stream().collect(Collectors.groupingBy(Student::getSurname));
+	}
+
 	/**
 	 * Преобразование матрицы (двумерный массив) в список.
 	 * @param matrix Матрица.
--- 1.Trainee/004.Lambda/src/test/java/ru/job4j/lambda/StreamAPIUtilTest.java
+++ 1.Trainee/004.Lambda/src/test/java/ru/job4j/lambda/StreamAPIUtilTest.java
@@ -3,6 +3,7 @@ package ru.job4j.lambda;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,6 +11,45 @@ import static org.junit.Assert.assertEquals;
  * Проверка различных методов, работающих с Stream API.
  */
 public class StreamAPIUtilTest {
+	/**
+	 * Тест преобразования списка в карту.
+	 */
+	@Test
+	public void testConvert() {
+		List<Student> students = List.of(
+				new Student("Петров", 12),
+				new Student("Сидоров", 12),
+				new Student("Иванов", 12),
+				new Student("Сидоров", 10),
+				new Student("Иванов", 10),
+				new Student("Васичкин", 12),
+				new Student("Толстой", 20),
+				new Student("Петров", 15),
+				new Student("Толстой", 21)
+		);
+
+		Map<String, List<Student>> expected = Map.ofEntries(
+				Map.entry("Петров", List.of(
+						new Student("Петров", 12),
+						new Student("Петров", 15))
+				),
+				Map.entry("Сидоров", List.of(
+						new Student("Сидоров", 12),
+						new Student("Сидоров", 10))
+				),
+				Map.entry("Иванов", List.of(
+						new Student("Иванов", 12),
+						new Student("Иванов", 10))
+				),
+				Map.entry("Васичкин", List.of(new Student("Васичкин", 12))),
+				Map.entry("Толстой", List.of(
+						new Student("Толстой", 20),
+						new Student("Толстой", 21))
+				)
+		);
+		assertEquals(expected, new StreamAPIUtil().listToMap(students));
+	}
+
 	/**
 	 * Проверка преобразования матрицы в список.
 	 */
